7f99ce7744b739ca4dbe73baf241024a83dc6998
Wishwajeet <Wishwajeetrana1990@gmail.com>
2026-08-12 13:45:49 +0530

    addedd types of modules in  modules file

--- 2. what is module.py
+++ 2. what is module.py
@@ -2,3 +2,10 @@ print("What is module?")
 print("A module is a file containing \
 Python definitions and statements. \
 The file name is the module name with the suffix .py added.")
+
+print("Tyepes of modules:")
+print("1. Built-in modules")
+print("2. User-defined modules")
+
+print("Built-in modules are those that come with Python.")
+print("User-defined modules are those that are created by the user.")
