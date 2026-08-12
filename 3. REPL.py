cc5fa0bb9fb87ef216a2355c7252ecf7a284f2b5
Wishwajeet <Wishwajeetrana1990@gmail.com>
2026-08-12 14:00:17 +0530

    added two new file named REPL and comment that explain the defination of the comment and REPL

---
+++ 3. REPL.py
@@ -0,0 +1,7 @@
+print("REPL")
+print("REPL stands for Read-Eval-Print Loop.")
+print("it is an itractive programming \
+environment that takes single user input (READ ),\
+evaluates the input (EVAL), and returs the result to the user (PRINT)")
+
+
---
+++ 4. comment in python.py
@@ -0,0 +1,12 @@
+print("What are comments in python?")
+print("Comment are the lines of code that are used to explain\
+the code and we don't want it to execute. these are called commments. \
+In python, comments start with a #")
+
+print("type of comments in python:")
+print("1. Single line comment")
+print("2. Multi line comment")
+print("Single line comment is a comment that is written in a single line. \
+It starts with a # and is followed by the comment text. ")
+print("Multi line comment is a comment that is written in multiple lines. \
+It starts with a # and is followed by the comment text. ")
