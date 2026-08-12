d3a5938b2f640186c25d91adf1fad4c52b243822
Wishwajeet <Wishwajeetrana1990@gmail.com>
2026-08-12 13:43:13 +0530

    add the new file named what  is pip and what is module .These file contains the defination of the pip and modules saparatly /

---
+++ 2. what is module.py
@@ -0,0 +1,4 @@
+print("What is module?")
+print("A module is a file containing \
+Python definitions and statements. \
+The file name is the module name with the suffix .py added.")
---
+++ 3. what is pip.py
@@ -0,0 +1,5 @@
+print("what is pip?")
+print("pip is a package management system\
+the is used to install and nanage software packages written in python \
+by someone else. It is a recursive acronym that stands for \
+'Pip Installs Packages' or 'Pip Installs Python'.")
