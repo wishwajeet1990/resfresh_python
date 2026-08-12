029c75b83ce548b432f204f8a786e05032df89ac
Wishwajeet <Wishwajeetrana1990@gmail.com>
2026-08-12 14:58:08 +0530

    Add a new file that let you know how to create virtual evs nad activate it ?

---
+++ 5. Creat virtual envs.py
@@ -0,0 +1,16 @@
+print("How to create virtual environment in python?")
+print("""To create a virtual environment in python
+    we can use the following commands :
+    1. python -m venv <name of the virtual environments >""")
+print("Activate the virtual environment ")
+print("""1 . For windows : <name of the virtual environments >\Scripts\activate.ps1 from power shell 
+        or <name of the virtual environments >\Scripts\activate.bat from vsc code terminal""")
+print("2 . For linux : source <name of the virtual environments >/bin/activate")
+print("Deactivate the virtual environment ")
+print("1 . For windows : <name of the virtual environments >\Scripts\deactivate.bat")
+print("2 . For linux : deactivate")
+print("""To delete the virtual environment just 
+        delete the folder of the virtual environment""")
+print("""if error occure while executing the commads then 
+        run the following command in power shell 
+        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser""")
