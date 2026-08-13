85277ad16a7e8fb3f2b16a7b1e2d35c3babb7cbc
Wishwajeet <Wishwajeetrana1990@gmail.com>
2026-08-13 12:14:51 +0530

    This file let you know the basic about the the assignment operator

---
+++ 10. Assignment Operation.py
@@ -0,0 +1,40 @@
+print("Airthmetic operations in python ")
+print("+ for addition \n- for subtraction \n* for multiplication \n/ for division \n% for modulus/remainder \n** for exponentiation or power calculation \n// for floor division \n")
+
+print("Comparision operations in python ")
+print("== for equal to \n!= for not equal to \n< for less than \n> for greater than \n<= for less than or equal to \n>= for greater than or equal to \n")
+
+print("Assignment operator in python ")
+print("= for assign the value to the variable ")
+
+print("Compund assignment operator in python")
+print("+= for addition and assign the value to the variable \n-= for subtraction and assign the value to the variable \
+      \n*= for multiplication and assign the value to the variable \n/= for division and assign the value to the variable \
+      \n%= for modulus and assign the value to the variable \n**= for exponention and assign the value to the variable \
+      \n//= floor division and assign the value to the variable \n")
+
+print("Logical operations in python ")
+print("and for logical AND \nor for logical OR \nnot for logical NOT \n")
+
+print("Bitwise operation in python")
+print("& for bitwise AND \n | for bitwise OR \n^ for bitwise XOR \n~ for bitwise NOT \n<< for left shift \n>> for right shift \n")
+
+print("Membership operation in python")
+print("in =  for check the membership of the value in the sequence \nnot in = for check the non-membership of the value in the sequence \n")
+print("Identity operator in python")
+print("is =  for check the identity of the value in the sequence \nis not = for check the non-identity of the value in the sequence\n")
+print("Identity operator is basically used to check the memory location of the variable .\nIt returns true if both the variables are pointing to the same memory location otherwise it returns false \n")
+print("Diffrence between == and is operator:")
+print("== operator is used to check the values of the variable whereas is operator is used to check the memory location of the variable \n")
+print("Operator precedence in python")
+print("Operator precedence is the order in which the operator is evaluate in an expression .")
+print("The operator with higher precedence is evaluated first and the operator with lower precedence is evaluate later. \n")
+print("if the two or more operator have the same precedence then the associativity of the operator is used to determine the order of evaluation .\n")
+print("The associativity of the operator can be left to right or right to left .\n")
+print("The operator with left to right associativity is evaluated from left to right and the operator with right to left associativity is evaluated from right to left .\n")
+print("The operator precedence in python is as follows : \n")
+print("1. () parenthesis \n2. **exponentiation \n3. +x, -x, ~x unary plus, unary minus, bitwise NOT \n4. *, /, //, % multiplication, division, floor division, modulus \n\
+      5. +, - addition, subtraction \n6. <<, >> bitwise shift operators \n7. & bitwise AND \n8. ^ bitwise XOR \n9. | bitwise OR \n10. ==, !=, >, <, >=, <= comparison operators \n\
+      11. =, +=, -=, *=, /=, %=, **=, //= compund Assignment operator \n")
+
+print("Most of the operator go from left to right")
