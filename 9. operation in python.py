248f95dba6e3241cbf06f0e6138fe5aea8df3fa1
Wishwajeet <Wishwajeetrana1990@gmail.com>
2026-08-13 12:16:10 +0530

    This file let you know the operaotr on python

---
+++ 9. operation in python.py
@@ -0,0 +1,14 @@
+a = 6
+b = 3
+print("a = ",a)
+print("b = ",b)
+
+print("a+b = ",a+b)
+print("a-b = ",a-b)
+print("a*b = ",a*b)
+print("a/b = ",int(a/b)) #a division in python always return float value so we need to tyecast it to int if we want integer value
+print("a/b = ",a/b) # it will return float value
+print("a%b = ",a%b)
+print("a**b = ",a**b)
+print("a//b = ",a//b)   #it will return integer value of division
+
