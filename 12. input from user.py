9880c6c5997f1a2cf157c304f2ba0676f836ab0c
Wishwajeet <Wishwajeetrana1990@gmail.com>
2026-08-19 17:10:41 +0530

    this files tell us how to take input from user

---
+++ 12. input from user.py
@@ -0,0 +1,5 @@
+name  = input("Enter your name ")
+
+#in this the input will capture the input from the use and assing it to the name variable 
+# now everything what we capture using input method it will be treated as string litral 
+print(name)
