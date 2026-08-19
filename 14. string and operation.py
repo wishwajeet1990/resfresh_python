ae9bdc53b50f07a8f3421b314c5a842b0d2ef858
Wishwajeet <Wishwajeetrana1990@gmail.com>
2026-08-19 17:12:26 +0530

    this files tell us the string and onperation that can be performed on string

---
+++ 14. string and operation.py
@@ -0,0 +1,25 @@
+# A string is a sequence of character terminated by null character enclosed in single quote double quote or tripple quote
+
+Name = "Wishwjaeet singh"
+
+print(Name)
+
+print("Accesign the element of the sting")
+
+print(Name[2])
+
+print("Assignment not possible in string")
+
+# Name[3] = "w"
+
+print(Name)
+
+print("1. A string is immutable ")
+print("2. A string a sequence of character terminated by nl character")
+
+print("Accessing the string with indeces")
+print(Name[1:])     #same as Name[Starting element : ending elemetn: step by which it jump ]
+
+print("Negative indeces")
+
+print(Name[-4:-1])
