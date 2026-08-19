5e4b494de0ab843a54a18faad3edbddaf297a2ec
Wishwajeet <Wishwajeetrana1990@gmail.com>
2026-08-19 17:09:57 +0530

    file tell how to typecast one variable to another variable

---
+++ 11. typecasting.py
@@ -0,0 +1,14 @@
+print("What is type function ?")
+print("A type fucntion is used to find out the data type of the variable .")
+a = "3456"
+
+print("Type of a is ",type(a))
+
+a = int(a)
+print("Type of a is ",type(a))
+print(a+5)
+
+print("what is typecasting ?")
+print("A typecasting is a technique to conver a data type to another data type if possible .")
+print("HOw to conver ?")
+print("int(variable) =  convert to integer type\nstr(variable) = convert to string\nfloat(variable) = conver to float\n ")
